12075a183627c8fef12729d2bac618863956bd6a
shriniketan0201 <62455796+shriniketan0201@users.noreply.github.com>
2023-01-24 11:33:24 +0530

    Add files via upload

---
+++ submit.sh
@@ -0,0 +1,31 @@
+make
+
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017B/220215_161814/0000/  data_b000.root -1 1000 log_datab0
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017B/220215_161814/0001/  data_b001.root -1 1000 log_datab1     
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017B/220215_161814/0002/  data_b002.root -1 1000 log_datab2       
+
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017C/220215_161833/0000/  data_c000.root -1 1000 log_datac0       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017C/220215_161833/0001/  data_c001.root -1 1000 log_datac1      
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017C/220215_161833/0002/  data_c002.root -1 1000 log_datac2       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017C/220215_161833/0003/  data_c003.root -1 1000 log_datac3       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017C/220215_161833/0004/  data_c004.root -1 1000 log_datac4       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017C/220215_161833/0005/  data_c005.root -1 1000 log_datac5
+
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017D/220215_161854/0000/  data_d000.root -1 1000 log_datad0       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017D/220215_161854/0001/  data_d001.root -1 1000 log_datad1       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017D/220215_161854/0002/  data_d002.root -1 1000 log_datad2       
+
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017E/220215_161916/0000/  data_e000.root -1 1000 log_datae0       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017E/220215_161916/0001/  data_e001.root -1 1000 log_datae1       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017E/220215_161916/0002/  data_e002.root -1 1000 log_datae2       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017E/220215_161916/0003/  data_e003.root -1 1000 log_datae3       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017E/220215_161916/0004/  data_e004.root -1 1000 log_datae4       
+
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017F/220215_161936/0000/  data_f000.root -1 1000 log_dataf0       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017F/220215_161936/0001/  data_f001.root -1 1000 log_dataf1       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017F/220215_161936/0002/  data_f002.root -1 1000 log_dataf2       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017F/220215_161936/0003/  data_f003.root -1 1000 log_dataf3       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017F/220215_161936/0004/  data_f004.root -1 1000 log_dataf4       
+./MakeCondorFiles.sh MyClass.exe root://cms-t2-se01.sdfarm.kr:1096//store/user/sdogra/Summer20ULMiniAOD_Dec2021/2017_v2/MiniAODv2/SinglePhoton/crab_job_SinglePho2017F/220215_161936/0005/  data_f005.root -1 1000 log_dataf5       
+
+
